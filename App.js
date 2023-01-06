541b7f89d3925922dc08f7da317b23e749948f25
hoyen204 <hoyen204@gmail.com>
2023-01-06 13:40:25 +0700

    Added release

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './app/components/bottomTab/BottomTab'
+import 'react-native-gesture-handler'
 
 const App = () => {
     return (
